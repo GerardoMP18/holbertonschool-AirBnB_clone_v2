75342ad78dc4e70e1a23316107725c7c48b93582
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-11 02:55:41 -0500

    [feat] Adding method for the List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Flask web application: HBNB """
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def session_close(exception):
+    """
+    List of states
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    List of states
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
