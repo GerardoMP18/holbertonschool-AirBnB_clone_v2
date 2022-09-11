f45acfaf755e9032beaea9d9c590dbb00cd7ff0e
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-11 03:11:28 -0500

    [feat]: Adding method to pass the route cities_by_states to be able to visualize states with their cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Flask web application: HBNB """
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def session_close(exception):
+    """
+    method to close the session of the db
+    """
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """
+    method to pass the route cities_by_states to be
+    able to visualize states with their cities
+    """
+    states = storage.all(State).values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
